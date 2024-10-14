b527c32e966814a88cf99b606782c555e061e908
Kevyn Nathan <kevynn.nathann@gmail.com>
2024-10-14 15:54:40 -0300

    manipulação arquivos

--- screen-match/screen-match.php
+++ screen-match/screen-match.php
@@ -57,3 +57,7 @@ $posicaoDoisPontos = strpos($filme['nome'],'-');
 var_dump($posicaoDoisPontos);
 
 var_dump(substr($filme['nome'], 0, $posicaoDoisPontos));
+
+$filmeComoStringJson = json_encode($filme);
+
+file_put_contents(__DIR__ . "/filme.json", $filmeComoStringJson);
