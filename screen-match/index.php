bb726d2712bf857e043d14fc2aba01e7e76c8f99
Kevyn Nathan <kevynn.nathann@gmail.com>
2024-11-07 17:22:28 -0300

    traits, namespaces e autoload

--- screen-match/index.php
+++ screen-match/index.php
@@ -1,13 +1,9 @@
 <?php
 
-require __DIR__ . "/src/Modelo/Avaliavel.php";
-require __DIR__ . "/src/Modelo/Genero.php";
-require __DIR__ . "/src/Modelo/Titulo.php";
-require __DIR__ . "/src/Modelo/Episodio.php";
-require __DIR__ . "/src/Modelo/Serie.php";
-require __DIR__ . "/src/Modelo/Filme.php";
-require __DIR__ . "/src/Calculos/CalculadoraDeMaratona.php";
-require __DIR__ . "/src/Calculos/ConversorNotaEstrela.php";
+require "autoload.php";
+
+use ScreenMatch\Modelo\{Filme, Episodio, Serie, Genero};
+use ScreenMatch\Calculos\{CalculadoraDeMaratona, ConversorNotaEstrela};
 
 echo "Bem-Vindo(a) ao ScreenMatch \n";
 
--- screen-match/src/Modelo/Titulo.php
+++ screen-match/src/Modelo/Titulo.php
@@ -1,28 +1,19 @@
 <?php
 
-abstract class Titulo implements Avaliavel {
-        
-    private array $notas;
+namespace ScreenMatch\Modelo;
 
+abstract class Titulo implements Avaliavel {
+
+    use ComAvaliacao;
+        
     public function __construct(
         public readonly string $nome,
         public readonly int $anoLancamento,
         public readonly Genero $genero,
     ) {
-        $this->notas = [];
+
     }
 
-    public function avalia(float $nota): void {
-        $this->notas[] = $nota;
-    }
-
-    public function media(): float {
-
-        $somaNotas = array_sum($this->notas);
-        $quantidadeNotas = count($this->notas);
-
-        return $somaNotas / $quantidadeNotas;
-    }
 
     abstract public function duracaoEmMinutos(): int;
 }
