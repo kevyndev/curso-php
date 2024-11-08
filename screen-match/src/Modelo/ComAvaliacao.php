fc1fa80ec7b9ddf519c1c07f718db032bf98937f
Kevyn Nathan <kevynn.nathann@gmail.com>
2024-11-07 21:47:45 -0300

    validando excecoes

--- screen-match/src/Modelo/ComAvaliacao.php
+++ screen-match/src/Modelo/ComAvaliacao.php
@@ -2,11 +2,24 @@
 
 namespace ScreenMatch\Modelo;
 
+use ScreenMatch\Exception\NotaInvalidaException;
+
 trait ComAvaliacao {
 
     private array $notas = [];
 
+    /**
+     * Summary of avalia
+     * @param float $nota
+     * @throws NotaInvalidaException se a nota for negativa ou maior que 10
+     * @return void
+     */
     public function avalia(float $nota): void {
+
+        if ($nota < 0 || $nota > 10) {
+            throw new NotaInvalidaException();
+        }
+
         $this->notas[] = $nota;
     }
 
